1f84efdc0148c21e91328d84c0e6f0076bad2b19
Antonio Alzate <antonioalzate7@gmail.com>
2022-01-10 17:08:07 -0500

    Firebase deploy.

---
+++ build/precache-manifest.0d8ca51e40442957775966251f506f72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86279570f0a617151178fe23644a534b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bda854ccfdbafc177103",
+    "url": "/static/css/main.ccdb0674.chunk.css"
+  },
+  {
+    "revision": "4a0aa8be4ee1550506da",
+    "url": "/static/js/2.c0f02d4d.chunk.js"
+  },
+  {
+    "revision": "bda854ccfdbafc177103",
+    "url": "/static/js/main.3cd087f4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
